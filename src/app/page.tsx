1c701860a7620fa22d51147547e6bc9c96c928a1
poncho29 <meneses0828@gmail.com>
2024-06-20 21:39:24 -0500

    [ADD] - config fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { fontTitle } from "@/config/fonts";
 
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+
+      <h1 className={fontTitle.className}>Home page</h1>
     </main>
   );
 }
---
+++ src/config/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Montserrat_Alternates } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const fontTitle = Montserrat_Alternates({
+  subsets: ["latin"],
+  weight: ["500", "700"]
+});
