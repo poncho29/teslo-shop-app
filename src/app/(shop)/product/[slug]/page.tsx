14568491dc42a5d2a9898504eee9b315d0561f28
poncho29 <meneses0828@gmail.com>
2024-06-22 12:10:25 -0500

    [ADD] - layout product detail page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,45 @@
+import { notFound } from "next/navigation";
 
-export default function ProductPage() {
+import { titleFont } from "@/config/fonts";
+
+import { initialData } from "@/seed/seed";
+
+interface Props {
+  params: {
+    slug: string;
+  }
+}
+
+export default function ProductPage({ params }: Props) {
+  const { slug } = params;
+  const product = initialData.products.find(product => product.slug === slug);
+
+  if (!product) notFound();
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="grid gap-3 mt-5 mb-20 md:grid-cols-3 ">
+      <div className="col-span-1 md:col-span-2">
+        <h1>Product Page</h1>
+      </div>
+
+      <div className="col-span-1 px-5">
+        <h1
+          className={`${titleFont.className} antialiased font-bold text-xl`}
+        >
+          { product.title }
+        </h1>
+
+        <p className="text-lg mb-5">${ product.price }</p>
+
+        <button className="btn-primary my-5">
+          Agregar al carrito
+        </button>
+
+        <h3 className="text-sm font-bold">Descripción</h3>
+        <p className="font-light">
+          { product.description }
+        </p>
+      </div>
     </div>
   );
 }
