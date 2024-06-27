d0736d6508e6135357a97cfdfa4379500b446d4c
poncho29 <meneses0828@gmail.com>
2024-06-26 22:33:54 -0500

    [ADD] - create category table

---
+++ src/helpers/capitalize.ts
@@ -0,0 +1,6 @@
+export function capitalize(text: string): string {
+  return text
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,13 +1,30 @@
 import prisma from '../lib/prisma';
 
+import { capitalize } from '../helpers/capitalize';
+
 import { initialData } from "./seed";
 
-(async function main() {
+async function main() {
   await Promise.all([
     prisma.productImage.deleteMany(),
     prisma.product.deleteMany(),
     prisma.category.deleteMany(),
   ]);
 
+  //Categories
+  const { categories, products } = initialData;
+  const categoriesData = categories.map((name) => ({ name }));
+
+  console.log(categoriesData)
+  await prisma.category.createMany({
+    data: categoriesData
+  });
+
   console.log('Seed executed successfully');
+};
+
+(() => {
+  if ( process.env.NODE_ENV === 'production' ) return;
+
+  main();
 })();
