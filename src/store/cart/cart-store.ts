ce3d7563cde6a6dc3c54b65ce074056a7d9e5822
SebasMen <sebastian.meneses@antpack.com>
2024-07-05 19:54:05 -0500

    [ADD] - make cart persistent in localstorage

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -10,38 +10,43 @@ interface State {
 }
 
 export const useCartStore = create<State>()(
-  (set, get) => ({
-    cart: [],
-
-    // Methods
-    addProductToCart: (product: CartProduct) => {
-      const { cart } = get();
-
-      // Revisar si el producto existe en el carrito con la talla seleccionada
-      const productInCart = cart.some(
-        (item) => item.id === product.id && item.size === product.size
-      );
-
-      if (!productInCart) {
-        // Insertar el producto en el carrito
-        set({ cart: [...cart, product] });
-        return;
-      }
-
-      // El product existe en el carrito con la talla seleccionada
-      const updatedCartProducts = cart.map((item) => {
-        if (item.id === product.id && item.size === product.size) {
-          // Aumentar la cantidad del producto
-          return {
-            ...item,
-            quantity: item.quantity + product.quantity
-          }
+  persist(
+    (set, get) => ({
+      cart: [],
+  
+      // Methods
+      addProductToCart: (product: CartProduct) => {
+        const { cart } = get();
+  
+        // Revisar si el producto existe en el carrito con la talla seleccionada
+        const productInCart = cart.some(
+          (item) => item.id === product.id && item.size === product.size
+        );
+  
+        if (!productInCart) {
+          // Insertar el producto en el carrito
+          set({ cart: [...cart, product] });
+          return;
         }
-
-        return item;
-      });
-
-      set({ cart: updatedCartProducts });
+  
+        // El product existe en el carrito con la talla seleccionada
+        const updatedCartProducts = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            // Aumentar la cantidad del producto
+            return {
+              ...item,
+              quantity: item.quantity + product.quantity
+            }
+          }
+  
+          return item;
+        });
+  
+        set({ cart: updatedCartProducts });
+      }
+    }),
+    {
+      name: "shopping-cart",
     }
-  })
+  )
 )
