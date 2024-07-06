d347a237b9839c21bce7dde126f45936d5de25a2
SebasMen <sebastian.meneses@antpack.com>
2024-07-05 19:41:01 -0500

    [ADD] - create function to add product to cart

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -1,12 +1,46 @@
-import type { CartProduct } from "@/interfaces";
 import { create } from "zustand";
+
+import type { CartProduct } from "@/interfaces";
 
 interface State {
   cart: CartProduct[];
+
+  addProductToCart: (product: CartProduct) => void;
 }
 
 export const useCartStore = create<State>()(
-  (set) => ({
+  (set, get) => ({
     cart: [],
+
+    // Methods
+    addProductToCart: (product: CartProduct) => {
+      const { cart } = get();
+
+      // Revisar si el producto existe en el carrito con la talla seleccionada
+      const productInCart = cart.some(
+        (item) => item.id === product.id && item.size === product.size
+      );
+
+      if (!productInCart) {
+        // Insertar el producto en el carrito
+        set({ cart: [...cart, product] });
+        return;
+      }
+
+      // El product existe en el carrito con la talla seleccionada
+      const updatedCartProducts = cart.map((item) => {
+        if (item.id === product.id && item.size === product.size) {
+          // Aumentar la cantidad del producto
+          return {
+            ...item,
+            quantity: item.quantity + product.quantity
+          }
+        }
+
+        return item;
+      });
+
+      set({ cart: updatedCartProducts });
+    }
   })
 )
